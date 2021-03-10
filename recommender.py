a34294231eb3ef9d9571a995d9d43123dc490bfe
roberto1618 <roberto.otero161@gmail.com>
2021-03-10 17:39:39 +0100

    get the final recommendations

--- recommender.py
+++ recommender.py
@@ -3,13 +3,13 @@ import pandas as pd
 import numpy as np
 from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.models import Model
-from autoencoder import autoencoder_model
+#from autoencoder import autoencoder_model
 from data_clean import data_clean
 from scipy.sparse import csr_matrix
 from sklearn.model_selection import train_test_split
 
 # Autoencoder builder
-def autoencoder_model(x_train, x_test, epochs = 800, batch_size = 16, encoding_dim = 5, hidden_activation = 'relu', optimizer = 'adam', loss = 'binary_crossentropy'):
+def autoencoder_model(x_train, epochs = 800, batch_size = 16, encoding_dim = 5, hidden_activation = 'relu', optimizer = 'adam', loss = 'binary_crossentropy'): #x_test,
     input_dim = x_train.shape[1]
     input_layer = Input(shape = (input_dim,))
     encoding_layer = Dense(encoding_dim, activation = hidden_activation)
@@ -22,7 +22,7 @@ def autoencoder_model(x_train, x_test, epochs = 800, batch_size = 16, encoding_d
 
     autoencoder.compile(optimizer = optimizer, loss = loss)
 
-    autoencoder.fit(x_train, x_train, epochs = epochs, batch_size = batch_size, validation_data = (x_test, x_test), verbose = 1)
+    autoencoder.fit(x_train, x_train, epochs = epochs, batch_size = batch_size, verbose = 1) #, validation_data = (x_test, x_test)
 
     return autoencoder
 
@@ -55,15 +55,15 @@ data_sparse = csr_matrix((quantity, (rows, cols)), shape=(len(clients), len(item
 dense_matrix = np.array(data_sparse.todense())
 
 # Divide into train and test
-x_train = dense_matrix[:-4]
-x_test = dense_matrix[-4:]
+x_train = dense_matrix #[:-4]
+#x_test = dense_matrix[-4:]
 #x_train, x_test = train_test_split(dense_matrix, test_size = 0.2)
 
 # Build the model
-autoencoder = autoencoder_model(x_train, x_test)
+autoencoder = autoencoder_model(x_train) #, x_test
 
 # Get the prediction
-predicted_items = autoencoder.predict(x_test)
+predicted_items = autoencoder.predict(x_train) #x_test
 predictions_df = pd.DataFrame(predicted_items)
 
 # Add the client
@@ -75,7 +75,46 @@ predictions_melted = pd.melt(predictions_df, id_vars = ['client'], value_vars = 
 predictions_melted["rank"] = predictions_melted.groupby("client")["value"].rank("dense", ascending=False)
 
 # Final recommendations
-top_recommendations = predictions_melted[(predictions_melted['rank']>=1) & (predictions_melted['rank']<=5)].sort_values('client')
+n_recom = 5
+top_recommendations = predictions_melted[(predictions_melted['rank']>=1) & (predictions_melted['rank']<=n_recom)].sort_values('client')
 
-#top_recommendations = pd.merge(top_recommendations, data, left_on = 'variable', right_on = 'item_id_cat', how = 'inner')
+# Change the value of recommendation for the name
+items_data = data[['item', 'item_id_cat']].drop_duplicates()
+top_recommendations = pd.merge(top_recommendations, items_data, left_on = 'variable', right_on = 'item_id_cat', how = 'left')
+
+# Pivot table to have recommendations by column
+top_recommendations['rank'] = 'rec_' + top_recommendations['rank'].astype(int).astype(str)
+top_recommendations = top_recommendations.pivot(index = 'client', columns = 'rank', values = 'item')
+top_recommendations.columns.name = None
+top_recommendations = top_recommendations.reset_index()
+
+# Merge to with the data
+top_recommendations = pd.merge(data, top_recommendations, left_on = 'id_client_cat', right_on = 'client', how = 'inner')
+
+# Remove products that have been already purchased
+for r in range(1, n_recom + 1):
+    top_recommendations.loc[top_recommendations['item']==top_recommendations['rec_' + str(r)], 'rec_' + str(r)] = 'remove'
+
+# Join by customer
+for r in range(1, n_recom + 1):
+    top_recommendations['rec_' + str(r)] = top_recommendations.groupby(['id_client'])['rec_' + str(r)].transform(lambda x : ' '.join(x))
+    top_recommendations['rec_' + str(r)] = top_recommendations['rec_' + str(r)].str.split().apply(lambda x: ''.join(list(set(x))))
+    top_recommendations.loc[top_recommendations['rec_' + str(r)].str.contains("remove"), 'rec_' + str(r)] = None
+
+# Remove duplicates
+top_recommendations = top_recommendations[['id_client'] + ['rec_' + str(r) for r in range(1, n_recom + 1)]]
+top_recommendations = top_recommendations.drop_duplicates()
+
+# Get only the final recommendations
+top_recommendations = pd.melt(top_recommendations, id_vars = ['id_client'], value_vars = ['rec_' + str(r) for r in range(1, n_recom + 1)])
+top_recommendations = top_recommendations.loc[top_recommendations['value'].notnull(),:]
+top_recommendations['variable'] = top_recommendations['variable'].map(lambda x: x.lstrip('rec_')).astype(int)
+top_recommendations['rank'] = top_recommendations.groupby('id_client')['variable'].rank('dense')
+top_recommendations = top_recommendations.loc[top_recommendations['rank']<4,:]
+top_recommendations['rank'] = 'rec_' + top_recommendations['rank'].astype(int).astype(str)
+top_recommendations = top_recommendations.pivot(index = 'id_client', columns = 'rank', values = 'value')
+top_recommendations.columns.name = None
+top_recommendations = top_recommendations.reset_index()
+
+
 print(top_recommendations)
